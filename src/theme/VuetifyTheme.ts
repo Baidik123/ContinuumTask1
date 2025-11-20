37fd7da42111cb9ab8e2dfa263bf736bcc136579
Baidik123 <baidi.bora@ncircletech.com>
2025-11-20 11:36:09 +0530

    elevation changes

--- src/theme/VuetifyTheme.ts
+++ src/theme/VuetifyTheme.ts
@@ -4,31 +4,6 @@ import { aliases,mdi } from "vuetify/iconsets/mdi";
 import 'vuetify/styles';
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-// export default createVuetify({
-//     components,
-//     directives,
-//     icons: {
-//         defaultSet: 'mdi',
-//         aliases,
-//         sets: {
-//             mdi,
-//         },
-//     },
-//     theme: {
-//         defaultTheme: 'customTheme',
-//         themes: {
-//             customTheme: {
-//                 dark: false,
-//                 colors: {
-//                     primary:'#faf9f9ff',
-//                     secondary: '#424242',
-//                     accent: '#82B1FF',  
-//                     background: '#f9f9f9',
-//                 },
-//             },
-//         },
-//     },        
-// });
 export default createVuetify({
   components,
   directives,
